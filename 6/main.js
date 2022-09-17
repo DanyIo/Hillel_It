ef6fd7508907053ea015bb0b675ce34b1767e361
DanIlo <danylobodnar96@gmail.com>
2022-09-17 15:32:41 +0300

    brackets

--- 6/main.js
+++ 6/main.js
@@ -5,10 +5,14 @@ function thesmallestnumber(num1, num2, num3, num4) {
     return Math.min.apply(null, [num1, num2, num3, num4]);
   }
 }
-console.log(thesmallestnumber())
+console.log(thesmallestnumber());
 function greatestcommonden(x = "Введіть хоча б 1 число.", y) {
-  if (x < 0 || y < 0) return "Одне з введених чисел меньше за 0.";
-  if (y > x) return greatestcommonden(y, x);
+  if (x < 0 || y < 0) {
+    return "Одне з введених чисел меньше за 0.";
+  }
+  if (y > x) {
+    return greatestcommonden(y, x);
+  }
   return !y ? x : greatestcommonden(y, x % y);
 }
 console.log(greatestcommonden());
@@ -16,14 +20,16 @@ function perfectnumber(number) {
   if (number == undefined) {
     return "Введіть число.";
   } else {
-    if (number === 0) return false;
+    if (number === 0) {
+      return false;
+    }
     let k = 0;
     for (let i = 1; i <= number / 2; i++) {
       if (number % i === 0) {
         k += i;
       }
     }
-    return number === k ? "Число є досконалим." : "Число не є досконал.";
+    return number === k ? "Число є досконалим." : "Число не є досконалим.";
   }
 }
 console.log(perfectnumber());
@@ -31,7 +37,9 @@ function sumofarray(num1, num2) {
   if (num1 === undefined || num2 === undefined) {
     return "Введіть 2 числа.";
   } else {
-    if (num1 > num2) return sumofarray(num2, num1);
+    if (num1 > num2) {
+      return sumofarray(num2, num1);
+    }
     sum = 0;
     for (let i = num1; i <= num2; i++) {
       sum += i;
